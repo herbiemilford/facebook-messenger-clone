a026e0c9ade1bff5e06970b795df8e0f2b3f6a5b
Joe Crowder <toast.one.art@gmail.com>
2020-07-25 22:02:02 -0700

    add prompt

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
+import Message from "./Message";
 
 function App() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState(["hi", "hello", "whats up"]);
+  const [messages, setMessages] = useState([
+    { username: "sonny", text: "sir" },
+    { username: "conner", text: "whazzz up" }
+  ]);
+  const [username, setUsername] = useState("");
+  //useState = variable in REACT
+  //useEffect = run code on a condition in REACT
 
-  console.log(input);
-  console.log(messages);
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"));
+  }, []);
 
   const sendMessage = event => {
     event.preventDefault();
-    setMessages([...messages, input]);
+    setMessages([...messages, { username: username, text: input }]);
     setInput("");
   };
 
   return (
     <div className="App">
-      <h1>Hello Wizard!</h1>
+      <h1>Hello Wizards!</h1>
+      <h2>{username}</h2>
 
       <form>
-        <input value={input} onChange={event => setInput(event.target.value)} />
+        <FormControl>
+          <InputLabel htmlFor="my-input">Enter a message...</InputLabel>
 
-        <button type="submit" onClick={sendMessage}>
-          Send Message
-        </button>
+          <Input
+            value={input}
+            onChange={event => setInput(event.target.value)}
+          />
+
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send Message
+          </Button>
+        </FormControl>
       </form>
 
       {messages.map(message => (
-        <p>{message}</p>
+        <Message username={message.username} text={message.text} />
       ))}
     </div>
   );
