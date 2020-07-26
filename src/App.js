b7947a864e3d58d3c2cc945e1351f3a12906a0d5
Joe Crowder <toast.one.art@gmail.com>
2020-07-25 20:03:45 -0700

    initial test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["hi", "hello", "whats up"]);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = event => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Wizard!</h1>
+
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+
+        <button type="submit" onClick={sendMessage}>
+          Send Message
+        </button>
+      </form>
+
+      {messages.map(message => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
